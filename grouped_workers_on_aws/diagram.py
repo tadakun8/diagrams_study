182f2768745bdcf964dabcbeff3c879486587b0d
tadakun8 <tadakun8.lab@gmail.com>
2021-12-14 21:18:10 +0900

    feat: make groupd workers on aws

---
+++ grouped_workers_on_aws/diagram.py
@@ -0,0 +1,13 @@
+from diagrams import Diagram
+from diagrams.aws.network import ELB
+from diagrams.aws.compute import EC2
+from diagrams.aws.database import RDS
+
+with Diagram("Groupd Worker", direction="TB"):
+  ELB("lb") >> [
+    EC2("worker1"),
+    EC2("worker2"),
+    EC2("worker3"),
+    EC2("worker4"),
+    EC2("worker5"),
+  ] >> RDS("events")
